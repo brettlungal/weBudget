a2825d826041b585aecf64f642b106c626c4b655
XuanWang <wangx17@myumanitoba.ca>
2021-02-23 20:33:35 -0600

    add error prompt

--- app/src/main/java/com/comp3350/webudget/objects/Account.java
+++ app/src/main/java/com/comp3350/webudget/objects/Account.java
@@ -4,25 +4,21 @@ import java.util.ArrayList;
 
 public class Account {
 
-    private static int ID;
     private String fName;
     private String lName;
     private String username;
     private String password;
-    private int age;
     private Wallet wallet;
     private ArrayList<Group> groups;
 
-    public Account(String fName, String lName, int age ,String username, String password){
+    public Account(String fName, String lName, String username, String password){
         this.fName = fName;
         this.lName = lName;
-        this.age = age;
         this.wallet = wallet;
         this.username = username;
         this.password = password;
         this.wallet = new Wallet(username);
         this.groups = new ArrayList<Group>();
-        ID++;
     }
 
     public String getFirstName() {
@@ -31,10 +27,6 @@ public class Account {
 
     public String getLastName() {
         return lName;
-    }
-
-    public int getAge() {
-        return age;
     }
 
     public Wallet getWallet() {
@@ -48,10 +40,6 @@ public class Account {
     public void addToGroup(Group newGroup){
         groups.add(newGroup);
         newGroup.addMember(this);
-    }
-
-    public int getID(){
-        return ID;
     }
 
     public String getUsername(){
