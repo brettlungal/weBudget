c36610da06a595efdd3a22ebad89fb40e0756f3d
B Zacharias <zachar26@myumanitoba.ca>
2021-03-11 14:44:27 -0600

    messed with WalletPersistence to get both Dependancy Injection and Singleton design patterns working. Its ugly and probably bad...

--- app/src/main/java/com/comp3350/webudget/application/WalletPersistence.java
+++ app/src/main/java/com/comp3350/webudget/application/WalletPersistence.java
@@ -1,22 +1,31 @@
 package com.comp3350.webudget.application;
 
+import com.comp3350.webudget.persistence.AccountDatabase;
 import com.comp3350.webudget.persistence.IAccountDatabase;
 import com.comp3350.webudget.persistence.IWalletDatabase;
 import com.comp3350.webudget.persistence.TestAccountDatabase;
 import com.comp3350.webudget.persistence.TestWalletDatabase;
 
 public class WalletPersistence {
-    private static IWalletDatabase walletDB;
+    private static IWalletDatabase walletPersistence = null;
 
-    public WalletPersistence(IWalletDatabase injectedWalletDB){
-        walletDB = injectedWalletDB;
+    //dependancy injected singleton, where you only have to pass the class you want instantiated
+    //This is either genius, or a HORRIBLE hack
+    public static synchronized <IWD extends IWalletDatabase> IWalletDatabase getPersistence(Class<IWD> injectedWalletDB){
+        if (walletPersistence == null) {
+            try {
+                walletPersistence = injectedWalletDB.newInstance();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
+        return walletPersistence;
     }
 
-    public WalletPersistence(){
-        walletDB = new TestWalletDatabase();
+    public IWalletDatabase WalletPersistence(){
+        return getPersistence(TestWalletDatabase.class);
     }
 
-    public IWalletDatabase getDatabase(){
-        return walletDB;
-    }
 }
