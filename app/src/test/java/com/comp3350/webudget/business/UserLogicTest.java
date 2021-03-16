c19177515d753557a360a6c040d877b22f1023c9
B Zacharias <zachar26@myumanitoba.ca>
2021-03-16 17:10:34 -0500

    started refactoring IUserLogic tests

--- app/src/test/java/com/comp3350/webudget/business/UserLogicTest.java
+++ app/src/test/java/com/comp3350/webudget/business/UserLogicTest.java
@@ -1,5 +1,38 @@
 package com.comp3350.webudget.business;
 
+import com.comp3350.webudget.application.Services;
+import com.comp3350.webudget.objects.Account;
+import com.comp3350.webudget.persistence.IAccountDatabase;
+import com.comp3350.webudget.persistence.IWalletDatabase;
+import com.comp3350.webudget.persistence.TestAccountDatabase;
+import com.comp3350.webudget.persistence.TestWalletDatabase;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class UserLogicTest {
     //TODO flesh this out with tests
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private IAccountDatabase testAccountDB;
+    private IWalletDatabase testWalletDB;
+
+    @Before
+    public void setUp(){
+        testWalletDB = new TestWalletDatabase();
+        testAccountDB = new TestAccountDatabase(testWalletDB);
+        Services.accountPersistence().insertUser("testF1", "testL1", "user1", "password1");
+        Services.accountPersistence().insertUser("testF2", "testL2", "user2", "password2");
+    }
+
+    @Test(expected = SignupLogic.SignupException.class)
+    public void signupFail() throws SignupLogic.SignupException {
+        //String[] input = {"xx", "xx", "user", "password"};
+        //new SignupLogic(input);
+        //new SignupLogic(input);
+    }
 }
