1d8659c411860e02e38bf424e61d363f7b7b07b5
brogrammer39 <lungalb@myumanitoba.ca>
2021-03-16 16:40:51 -0500

    add dynamic balance and user population

--- app/src/main/java/com/comp3350/webudget/presentation/WalletFragment.java
+++ app/src/main/java/com/comp3350/webudget/presentation/WalletFragment.java
@@ -6,8 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.comp3350.webudget.application.AccountException;
 import com.comp3350.webudget.application.Services;
 import com.comp3350.webudget.R;
+import com.comp3350.webudget.application.WalletException;
 import com.comp3350.webudget.business.IUserWalletLogic;
 
 import androidx.annotation.NonNull;
@@ -18,17 +21,26 @@ import androidx.fragment.app.Fragment;
 public class WalletFragment extends Fragment implements View.OnClickListener {
 
 
-    private TextView balance;
+    private TextView balance,owner;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View walletView = inflater.inflate(R.layout.fragment_wallet , container, false);
         walletView.findViewById(R.id.send_button).setOnClickListener(this);
-        //IUserWalletLogic wallet_logic = Services.userwalletlogic();
+        String current_user = Services.userLogic().getCurrentUser();
+        double bal = 0;
+        try {
+            bal = Services.userWalletLogic().getAmount(current_user);
+        } catch (AccountException e) {
+            e.printStackTrace();
+        } catch (WalletException e) {
+            e.printStackTrace();
+        }
+        owner = (TextView) walletView.findViewById(R.id.owner);
         balance = (TextView) walletView.findViewById(R.id.balance);
-        balance.setText("$69.420");
-        //wallet_logic
+        owner.setText(current_user);
+        balance.setText(String.valueOf(bal));
 
 
         return walletView;
