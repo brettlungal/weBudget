3e749d2c23bc67ab9dffd6c81f62e3930b99be87
B Zacharias <zachar26@myumanitoba.ca>
2021-03-17 09:49:10 -0500

    updated IGroupLogic

--- app/src/main/java/com/comp3350/webudget/business/IGroupsLogic.java
+++ app/src/main/java/com/comp3350/webudget/business/IGroupsLogic.java
@@ -1,5 +1,6 @@
 package com.comp3350.webudget.business;
 
+import com.comp3350.webudget.application.AccountException;
 import com.comp3350.webudget.application.GroupException;
 import com.comp3350.webudget.objects.Group;
 
@@ -8,11 +9,11 @@ import java.util.ArrayList;
 public interface IGroupsLogic {
     public Group getGroup(int id)  throws GroupException;
     public ArrayList<Group> getGroups()  throws GroupException;
-    public ArrayList<Group> getUserGroups(String username)  throws GroupException;
-    public void createEmptyGroup(String name) throws GroupException;
-    public void createGroupWithUsers(String name, ArrayList<String> usernames)  throws GroupException;
-    public void addUserToGroup(String username, int groupID)  throws GroupException;
-    public void removeUserFromGroup(String username, int groupID)  throws GroupException;
+    public ArrayList<Group> getUserGroups(String username)  throws AccountException, GroupException;
+    public int createEmptyGroup(String name) throws GroupException;
+    public int createGroupWithUsers(String name, ArrayList<String> usernames)  throws AccountException, GroupException;
+    public void addUserToGroup(String username, int groupID)  throws AccountException, GroupException;
+    public void removeUserFromGroup(String username, int groupID)  throws AccountException, GroupException;
 
     //TODO add admins maybe?
 }
