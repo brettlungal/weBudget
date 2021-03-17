8a2bd88dc52e4f3f14562f3e7c0708b6013ce64a
brogrammer39 <lungalb@myumanitoba.ca>
2021-03-16 20:21:40 -0500

    add transferring funds between wallets

--- app/src/main/java/com/comp3350/webudget/presentation/WalletFragment.java
+++ app/src/main/java/com/comp3350/webudget/presentation/WalletFragment.java
@@ -22,6 +22,7 @@ public class WalletFragment extends Fragment implements View.OnClickListener {
 
 
     private TextView balance,owner;
+    private EditText username,amount;
 
     @Nullable
     @Override
@@ -37,6 +38,8 @@ public class WalletFragment extends Fragment implements View.OnClickListener {
         } catch (WalletException e) {
             e.printStackTrace();
         }
+        username = (EditText)walletView.findViewById(R.id.recipient_input);
+        amount = (EditText)walletView.findViewById(R.id.amount_input);
         owner = (TextView) walletView.findViewById(R.id.owner);
         balance = (TextView) walletView.findViewById(R.id.balance);
         owner.setText(current_user);
@@ -46,12 +49,31 @@ public class WalletFragment extends Fragment implements View.OnClickListener {
         return walletView;
     }
 
+    public String[] getInputValues(){
+        String[] values = new String[2];
+
+        values[0] = username.getText().toString();
+        values[1] = amount.getText().toString();
+
+        return values;
+    }
+
     @Override
     public void onClick(View v) {
 
         switch ( v.getId() ){
             //listen for click
-
+            case R.id.send_button:
+                String[] vals = getInputValues();
+                int amt = Integer.parseInt(vals[1]);
+                try {
+                    Services.userWalletLogic().deposit(vals[0],amt);
+                    Services.userWalletLogic().withdraw(Services.userLogic().getCurrentUser(),amt);
+                } catch (AccountException e) {
+                    e.printStackTrace();
+                } catch (WalletException e) {
+                    e.printStackTrace();
+                }
 
         }
 
