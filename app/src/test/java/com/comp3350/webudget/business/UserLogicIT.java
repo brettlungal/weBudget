43e1e21361d214e1902447e2a522bbc600ed7c6e
B Zacharias <zachar26@myumanitoba.ca>
2021-03-17 19:47:16 -0500

    added integration test file, but no tests

---
+++ app/src/test/java/com/comp3350/webudget/business/UserLogicIT.java
@@ -0,0 +1,12 @@
+package com.comp3350.webudget.business;
+
+import org.junit.Before;
+
+public class UserLogicIT {
+
+
+    @Before
+    public void setup(){
+        //Setup test HSQL database here
+    }
+}
