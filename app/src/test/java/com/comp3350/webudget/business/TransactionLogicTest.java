38df3755da5fa89115eb7e3ac70ee212bc91f9c2
brettlungal <lungalb@myumanitoba.ca>
2021-04-13 16:02:06 -0500

    make change to transaction logic test

--- app/src/test/java/com/comp3350/webudget/business/TransactionLogicTest.java
+++ app/src/test/java/com/comp3350/webudget/business/TransactionLogicTest.java
@@ -53,7 +53,7 @@ public class TransactionLogicTest {
         testTransactionDB = new TestTransactionDatabase();
         IUserLogic userLogic = new UserLogic(testAccountDB);
         IGroupLogic groupLogic = new GroupLogic(testAccountDB,testGroupDB,testMembershipDB);
-        IUserWalletLogic walletLogic = new UserWalletLogic(testAccountDB,testWalletDB);
+        IUserWalletLogic walletLogic = new UserWalletLogic(testAccountDB, new WalletLogic(testWalletDB));
 
         testTransactionLogic = new TransactionLogic(testWalletDB,testAccountDB, testGroupDB, testTransactionDB ,userLogic);
 
