d53552670da65cc673abc1f0c13eace519187d55
B Zacharias <zachar26@myumanitoba.ca>
2021-04-13 10:01:19 -0500

    restructured tests, removed useless ones

--- app/src/test/java/com/comp3350/webudget/AllTests.java
+++ app/src/test/java/com/comp3350/webudget/AllTests.java
@@ -1,24 +1,13 @@
 package com.comp3350.webudget;
 
-import com.comp3350.webudget.business.GroupLogicTest;
-import com.comp3350.webudget.business.UserLogicTest;
-import com.comp3350.webudget.business.UserWalletLogicTest;
-import com.comp3350.webudget.objects.Account_test;
-import com.comp3350.webudget.objects.Group_test;
-import com.comp3350.webudget.objects.Wallet_test;
-
-
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        UserLogicTest.class,
-        UserWalletLogicTest.class,
-        GroupLogicTest.class,
+        UnitTests.class,
+        IntegrationTests.class,
 })
 public class AllTests
 {
