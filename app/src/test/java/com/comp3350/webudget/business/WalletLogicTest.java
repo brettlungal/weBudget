56b68f9ba8a223b618dd1725201872db3da8bd9e
Thomas Livingstone <thomas.livingstone.1006@gmail.com>
2021-04-13 19:16:17 -0500

    walletlogic/groupwalletlogic testing

--- app/src/test/java/com/comp3350/webudget/business/WalletLogicTest.java
+++ app/src/test/java/com/comp3350/webudget/business/WalletLogicTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -30,10 +31,11 @@ public class WalletLogicTest {
     }
 
     @Test
-    public void testZeroBalance() throws WalletException {
-        when(mockWallet.getBalance()).thenReturn(0);
+    public void testBalance() throws WalletException {
         when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
-        assertEquals(0, walletLogic.getAmount(0));
+        when(mockWallet.getBalance()).thenReturn(0);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        assertEquals(0, walletLogic.getAmount(mockWallet.getWalletID()));
         verify(mockWallet).getBalance();
         verify(mockWalletPersistence).getWallet(anyInt());
     }
@@ -41,6 +43,87 @@ public class WalletLogicTest {
     @Test(expected = WalletException.class)
     public void testNegativeDeposit() throws WalletException {
         when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        Mockito.doThrow(new WalletException("Negative Deposit")).when(mockWalletPersistence).deposit(anyInt(), eq(-100));
+        walletLogic.deposit(mockWallet.getWalletID(), -100);
+        verify(mockWalletPersistence).deposit(anyInt(), anyInt());
+    }
 
+    @Test(expected = WalletException.class)
+    public void testZeroDeposit() throws WalletException {
+        when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        Mockito.doThrow(new WalletException("Zero Deposit")).when(mockWalletPersistence).deposit(anyInt(), eq(0));
+        walletLogic.deposit(mockWallet.getWalletID(), 0);
+        verify(mockWalletPersistence).deposit(anyInt(), anyInt());
+    }
+
+    @Test(expected = WalletException.class)
+    public void testLargeDeposit() throws WalletException {
+        when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        Mockito.doThrow(new WalletException("Large Deposit")).when(mockWalletPersistence).deposit(anyInt(), eq(987654321));
+        walletLogic.deposit(mockWallet.getWalletID(), 987654321);
+        verify(mockWalletPersistence).deposit(anyInt(), anyInt());
+    }
+
+    @Test
+    public void testGoodDeposit() throws WalletException {
+        when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        walletLogic.deposit(mockWallet.getWalletID(), 100);
+        verify(mockWalletPersistence).deposit(anyInt(), anyInt());
+    }
+
+    @Test(expected = WalletException.class)
+    public void testNegativeWithdraw() throws WalletException {
+        when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        Mockito.doThrow(new WalletException("Negative Withdraw")).when(mockWalletPersistence).withdraw(anyInt(), eq(-100));
+        walletLogic.withdraw(mockWallet.getWalletID(), -100);
+        verify(mockWalletPersistence).withdraw(anyInt(), anyInt());
+        verify(mockWalletPersistence).getWallet(anyInt());
+    }
+
+    @Test(expected = WalletException.class)
+    public void testZeroWithdraw() throws WalletException {
+        when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        Mockito.doThrow(new WalletException("Zero Withdraw")).when(mockWalletPersistence).withdraw(anyInt(), eq(0));
+        walletLogic.withdraw(mockWallet.getWalletID(), 0);
+        verify(mockWalletPersistence).withdraw(anyInt(), anyInt());
+        verify(mockWalletPersistence).getWallet(anyInt());
+    }
+
+    @Test(expected = WalletException.class)
+    public void testLargeWithdraw() throws WalletException {
+        when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        Mockito.doThrow(new WalletException("Large Withdraw")).when(mockWalletPersistence).withdraw(anyInt(), eq(987654321));
+        walletLogic.withdraw(mockWallet.getWalletID(), 987654321);
+        verify(mockWalletPersistence).withdraw(anyInt(), anyInt());
+        verify(mockWalletPersistence).getWallet(anyInt());
+    }
+
+    @Test(expected = WalletException.class)
+    public void testOverdraftWithdraw() throws WalletException {
+        when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        when(mockWallet.getBalance()).thenReturn(100);
+        walletLogic.withdraw(mockWallet.getWalletID(), 150);
+        verify(mockWallet).getBalance();
+        verify(mockWalletPersistence).withdraw(anyInt(), anyInt());
+        verify(mockWalletPersistence).getWallet(anyInt());
+    }
+
+    @Test
+    public void testGoodWithdraw() throws WalletException {
+        when(mockWalletPersistence.getWallet(anyInt())).thenReturn(mockWallet);
+        when(mockWallet.getWalletID()).thenReturn(0);
+        when(mockWallet.getBalance()).thenReturn(100);
+        walletLogic.withdraw(mockWallet.getWalletID(), 50);
+        verify(mockWallet).getBalance();
+        verify(mockWalletPersistence).withdraw(anyInt(), anyInt());
+        verify(mockWalletPersistence).getWallet(anyInt());
     }
 }
